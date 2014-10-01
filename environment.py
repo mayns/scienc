5be3dd3526e564f06343a0070bc377fc7eda893a
mayns <oksgorobets@gmail.com>
2014-10-01 22:48:50 +0400

    science fields constants

---
+++ environment.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'mayns'
+
+
+########### SCIENCE FIELDS ###########
+
+MATH = u'Математика, информатика и механика'
+PHYSICS = u'Физика и астрономия'
+CHEMISTRY = u'Химия'
+BIOLOGY = u'Биология и медицинская наука'
+EARTH = u'Науки о Земле'
+ENGINEERING = u'Инженерные науки'
+IT = u'Информационные технологии и вычислительные системы'
+
+SCIENCE_FIELDS = [MATH, PHYSICS, CHEMISTRY, BIOLOGY, EARTH, ENGINEERING, IT]
