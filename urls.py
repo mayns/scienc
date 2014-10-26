9b0e02ccc2c4e7745ef476ebd5ad18c7a48a22b4
mayns <oksgorobets@gmail.com>
2014-10-26 22:19:58 +0300

    mega new decorator for requests

--- urls.py
+++ urls.py
@@ -15,13 +15,13 @@ url_handlers = [
     (r"/not-found", cm_handlers.NotFoundHandler),
 
     # scientists
-    (r"/scientist/(\w+)", sc_handlers.ScientistHandler),
-    (r"/scientists", sc_handlers.ScientistsListHandler),
+    (r"/api/scientist/(\w+)", sc_handlers.ScientistHandler),
+    (r"/api/scientists", sc_handlers.ScientistsListHandler),
     (r"/ajax/scientist/login/", sc_ajax_handlers.AjaxScientistLoginHandler),
 
     # projects
-    (r"/project/(\w+)", pr_handlers.ProjectHandler),
-    (r"/projects", pr_handlers.ProjectsListHandler),
+    (r"/api/project/(\w+)", pr_handlers.ProjectHandler),
+    (r"/api/projects", pr_handlers.ProjectsListHandler),
     # (r"/ckeditor/samples/", CkeditorSampleHandler),
 
 ]
