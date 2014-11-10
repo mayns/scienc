633feb264892c76cc2ca5ce833b7133562658bdc
mayns <oksgorobets@gmail.com>
2014-11-11 01:43:27 +0300

    clean up

--- mainScience.py
+++ mainScience.py
@@ -17,13 +17,11 @@ class ScienceApplication(web.Application):
     def __init__(self, xsrf_cookies=False):
         handlers = url_handlers
         settings_app = dict(
-            # static_path=settings.STATIC_PATH,
             debug=True,
             xsrf_cookies=xsrf_cookies,
             cookie_secret='fwwquydg367tgdbkjxlw362783t54%^I^&fcdsvjsxasaxs'
         )
         web.Application.__init__(self, handlers, **settings_app)
-        # self.template_loader = jinja_template_loader
 
 
 if __name__ == "__main__":
