08d15b6a318367660e26d0a9a3516b153c314595
mayns <oksgorobets@gmail.com>
2014-11-20 01:58:02 +0300

    init

--- common/psql_relations.py
+++ common/psql_relations.py
@@ -45,7 +45,7 @@ def create_db():
 @gen.coroutine
 def create_relations():
     try:
-        yield delete_tables()
+        # yield delete_tables()
         yield create_scientists_relation()
         yield create_project_relation()
         yield create_country_relation()
