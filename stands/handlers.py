820def34974f86a05f382d0c26847cbdf1f6fd4a
mayns <oksgorobets@gmail.com>
2015-04-12 21:58:14 +0300

    projects

--- stands/handlers.py
+++ stands/handlers.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from tornado import gen
 from base.handlers import BaseRequestHandler
+from project.project_bl import ProjectBL
 
 __author__ = 'mayns'
 
 
 class ServerGenTemplateHandler(BaseRequestHandler):
+
+    @gen.coroutine
     def get(self, *args, **kwargs):
-        self.render("projects_list.html", name="Alice", where=u'in Wonderland')
+        projects = yield ProjectBL.get_all()
+        self.render("projects_list.html", projects=projects)
