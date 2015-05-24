528666465777d0959e9d07fe2f5b15761b01ca8c
mayns <oksgorobets@gmail.com>
2015-05-25 00:42:21 +0300

    template handlers

--- benchmarks/urls_async.py
+++ benchmarks/urls_async.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from benchmarks import handlers_async
+from stands import handlers
 
 __author__ = 'mayns'
 
 url_handlers = [
     (r"/api/projects(/\w+)?", handlers_async.ProjectsHandler),
     (r"/api/user", handlers_async.UserHandler),
+    (r"/api/stand/items", handlers.ServerGenTemplateItemsHandler),
+    (r"/api/stand/item(/\d+)?", handlers.ServerGenTemplateItemHandler),
 ]
